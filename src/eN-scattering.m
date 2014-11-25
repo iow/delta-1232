ab3ca1a24ef0a7e33e4c1bac1db8bff35390529e
Volodymyr Shubnyi <iow@localhost.localdomain>
2014-11-25 10:59:08 +0100

    Redefined spinors, to follow Jacob-Wick convention... New infrastructure for calculation of spinors Various improvements Documentation pieces

--- src/eN-scattering.m
+++ src/eN-scattering.m
@@ -1,51 +1,75 @@
 (* ::Package:: *)
 
 chiFirst::usage =
-	"Two-component spinor for first \
+"\t chiFirst[helicity] - for spin in positive z-axis direction
+\t chiFirst[theta, phi, helicity] - for spin in positive (theta, phi) direction
+theta:    polar angle of scattering \
+(for in-particle use zero). \n\
+phi:      azimuthal angle of scattering \
+(for in-particle use zero). \n\
+helicity: spin projection over (theta, phi) axis. \n
+Two-component spinor for first \
 particle of 2->2 process. \n\
 Defined in c.m. reference frame.
 This definition follows Jacob-Wick convention \
 for spinors. \
 (second state will be with opposite up/down \
-conventions).\n\n
-theta:    polar angle of scattering \
-(for in-particle use zero). \n\
-phi:      azimuthal angle of scattering \
-(for in-particle use zero). \n\
-helicity: spin projection over (theta, phi) axis.";
-chiFirst[theta_, phi_, helicity_] :=
-	Switch[helicity,
+conventions).";
+
+chiFirst[h_]:=
+	Switch[h,
 		+1/2,
-			{Cos[theta/2], Exp[I phi] Sin[theta/2]},
+			{1, 0},
 		-1/2,
-			{-Exp[-I phi] Sin[theta/2], Cos[theta/2]},
-		_,
-			Message[chiFirst::invhel, helicity]
-		];
-chiFirst[helicity_] := chiFirst[0, 0, helicity];
-chiFirst::invhel="Helicity `1` is not equal to +1/2 or -1/2";
+			1/2 (PauliMatrix[1] - I PauliMatrix[2]).chiFirst[+1/2]
+	];
+
+chiFirst[theta_, phi_, h_]:=
+	Return[MatrixExp[-I phi PauliMatrix[3] / 2]
+		 . MatrixExp[-I theta PauliMatrix[2] / 2]
+		 . MatrixExp[+I phi PauliMatrix[3] / 2] . chiFirst[h]
+	//Simplify
+	];
+
 
 								
 chiSecond::usage =
-	"Two-component spinor for second \
-particle of 2->2 process. \n\
-Defined in c.m. reference frame.
-This definition follows Jacob-Wick convention \
-for spinors. \
-(first state will be with opposite up/down \
-conventions).\n\n
+"\t chiSecond[helicity] - for spin in negative z-axis direction
+\t chiSecond[theta, phi, helicity] - for spin in negative (theta, phi) direction
 theta:    polar angle of scattering \
 (for in-particle use zero). \n\
 phi:      azimuthal angle of scattering \
 (for in-particle use zero). \n\
-helicity: spin projection over (theta, phi) axis.";
-chiSecond[theta_, phi_, helicity_] :=
-	chiFirst[theta, phi, -helicity];
-chiSecond[helicity_] := chiSecond[0, 0, helicity];
+helicity: spin projection over (theta, phi) axis.
+
+Two-component spinor for second \
+particle of 2->2 process. \n\
+Defined in c.m. reference frame.
+This definition follows Jacob-Wick convention \
+for spinors -- rather specific \
+(first state will be with opposite up/down \
+conventions, but it is not all things!).\n
+For further references consult M. Jacob, G.C. Wick, \
+Annals of Physics: 7, 404-428 (1959) (this paper has reprint, \
+but it has minor typos). ";
+
+chiSecond[h_]:=
+	Return[MatrixExp[-I Pi PauliMatrix[2] / 2]
+		 . chiFirst[h]
+	//Simplify
+	];
+
+chiSecond[theta_, phi_, h_]:=
+	Return[MatrixExp[-I phi PauliMatrix[3] / 2]
+		 . MatrixExp[-I theta PauliMatrix[2] / 2]
+		 . MatrixExp[+I phi PauliMatrix[3] / 2] . chiSecond[h]
+	//Simplify
+	];
 
 
 polarVector::usage =
-	"Converts vector from polar to Cartesian coordinates \n
+"\t polarVector[abs,theta,phi]
+Converts vector from polar to Cartesian coordinates \n
 abs: absolute value of the vector.
 theta: polar angle.
 phi: azimuthal angle.";
@@ -58,61 +82,84 @@ pauliVector:= Table[PauliMatrix[k],{k,3}];
 
 
 leptonOut::usage =
-	"Outcoming 1/2-spin lepton state spinor in certain helicity state.
+"\t leptonOut[mn, s, theta, phi, helicity]
+mn:       mass of the compliment scattering particle \
+(mass of the lepton is taken zero)\n\
+s:        Mandelstam invariant in c.m. frame\
+theta:    polar angle of scattering \
+(for in-particle use zero). \n\
+phi:      azimuthal angle of scattering \
+(for in-particle use zero). \n\
+helicity: spin projection over (theta, phi) axis.
+Outcoming 1/2-spin lepton state spinor in certain helicity state.
 Taken in the mL<<mN limit.
 For using in X->eN process, where N is nucleon with mass \
 mN (either proton or neutron)";
-leptonOut[mN_, s_, theta_, phi_, helicity_]:=
+leptonOut[mn_, s_, theta_, phi_, h_]:=
 	Block[
 		{  
 			mL = 0,
-			k0 = (s - mN^2)/(2 Sqrt[s]),
-			kAbs = (s - mN^2)/(2 Sqrt[s])
+			k0 = (s - mn^2)/(2 Sqrt[s]),
+			kAbs = (s - mn^2)/(2 Sqrt[s])
 		},
 			Return[Sqrt[k0 + mL]
-					*Join[chiFirst[theta,phi,helicity],
-					      2 * helicity * kAbs *
-					       chiFirst[theta,phi,helicity]/(k0 + mL)]
+					*Join[chiFirst[theta, phi, h],
+					      2 * h * kAbs *
+					       chiFirst[theta, phi, h]/(k0 + mL)]
 					//Simplify[#, s>0 ]&
 			]
 	];
+
 leptonIn::usage =
-	"Incoming 1/2-spin lepton state spinor.
+"Incoming 1/2-spin lepton state spinor.
 Taken in the mL<<mN limit.
 For using in eN-> process, where N is nucleon with mass \
 mN (either proton or neutron)";
-leptonIn[mN_, s_, helicity_]:=
-	leptonOut[mN, s, 0, 0, helicity];
-leptonIn[mN_, s_, theta_, phi_, helicity_]:=
-	leptonOut[mN, s, theta, phi, helicity];
+
+leptonIn[mn_, s_, h_]:=
+	leptonOut[mn, s, 0, 0, h];
+
+leptonIn[mn_, s_, theta_, phi_, h_]:=
+	leptonOut[mn, s, theta, phi, h];
 
 
 nucleonOut::usage =
-	"Outcoming 1/2-spin nucleon state spinor in certain helicity state.
+"\t nucleonOut[mn, s, theta, phi, helicity]
+mn:       mass of this nucleon scattering particle \
+(mass of the compliment lepton is taken zero)\n\
+s:        Mandelstam invariant in c.m. frame\
+theta:    polar angle of scattering \
+(for in-particle use zero). \n\
+phi:      azimuthal angle of scattering \
+(for in-particle use zero). \n\
+helicity: spin projection over (theta, phi) axis.
+
+Outcoming 1/2-spin nucleon state spinor in certain helicity state.
 For using in X->eN process, where N is nucleon with mass \
 mN (either proton or neutron). Taken in the mL<<mN limit.";
-nucleonOut[mN_, s_, theta_, phi_, helicity_]:=
+
+nucleonOut[mn_, s_, theta_, phi_, l_]:=
 	Block[
 		{  
-			mL = 0,
-			p0 = (s + mN^2)/(2 Sqrt[s]),
-			pAbs = (s - mN^2)/(2 Sqrt[s])
+			ml = 0,
+			p0 = (s + mn^2)/(2 Sqrt[s]),
+			pAbs = (s - mn^2)/(2 Sqrt[s])
 		},
-			Return[Sqrt[p0 + mN]
-					* Join[chiSecond[theta,phi,helicity],
-					       2 * helicity * pAbs *
-							chiSecond[theta,phi,helicity]/(p0 + mN)]
-					//Simplify[#, s>0 && mN>0 && s > mN^2]&
+			Return[(-1)^(1/2 - l) Sqrt[p0 + mn]
+					* Join[chiSecond[theta, phi, l],
+					       2 * l * pAbs *
+							chiSecond[theta, phi, l]/(p0 + mn)]
+			//Simplify[#, s>0 && mn>0 && s > mn^2]&
 			]
 	];
 nucleonIn::usage =
 	"Incoming 1/2-spin nucleon state spinor in certain helicity state.
 For using in eN->X process, where N is nucleon with mass \
 mN (either proton or neutron). Taken in the mL<<mN limit.";
-nucleonIn[mN_, s_, helicity_]:=
-	nucleonOut[mN, s, 0, 0, helicity];
-nucleonIn[mN_, s_, theta_, phi_, helicity_]:=
-	nucleonOut[mN, s, theta, phi, helicity];
+nucleonIn[mn_, s_, l_]:=
+	nucleonOut[mn, s, 0, 0, l];
+nucleonIn[mn_, s_, theta_, phi_, l_]:=
+	nucleonOut[mn, s, theta, phi, l];
 
 
 g::usage="Minkowski metric tensor";
@@ -121,86 +168,192 @@ Subscript[g, mu_,nu_] :=
 		(*true*)  0,
 		(*false*) If[mu == 0, 1, -1]
 	]; 
+
 gamma::usage="Dirac gamma matrices (Dirac basis)";
-Subscript[gamma,0]={{1,0,0,0},{0,1,0,0},{0,0,-1,0},{0,0,0,-1}};
-Subscript[gamma,1]={{0,0,0,1},{0,0,1,0},{0,-1,0,0},{-1,0,0,0}};
-Subscript[gamma,2]={{0,0,0,-I},{0,0,I,0},{0,I,0,0},{-I,0,0,0}};
-Subscript[gamma,3]={{0,0,1,0},{0,0,0,-1},{-1,0,0,0},{0,1,0,0}};
+Subscript[gamma,0]:={{1,0,0,0},{0,1,0,0},{0,0,-1,0},{0,0,0,-1}};
+Subscript[gamma,1]:={{0,0,0,1},{0,0,1,0},{0,-1,0,0},{-1,0,0,0}};
+Subscript[gamma,2]:={{0,0,0,-I},{0,0,I,0},{0,I,0,0},{-I,0,0,0}};
+Subscript[gamma,3]:={{0,0,1,0},{0,0,0,-1},{-1,0,0,0},{0,1,0,0}};
+Subscript[gamma,5]:={{0,0,1,0},{0,0,0,1},{1,0,0,0},{0,1,0,0}};
+
 e::usage="Subscript[e,0] - 4x4 unit matrix"
-Subscript[e,0]={{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-Subscript[gamma,5]={{0,0,1,0},{0,0,0,1},{1,0,0,0},{0,1,0,0}};
+Subscript[e,0]:={{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
 
 sliced::usage="Feynman slash notation";
 sliced[p_]:= Sum[Subscript[g,mu,nu] Subscript[p,mu] Subscript[gamma,nu],{mu, 0, 3},{nu,0,3}];
-dotMinkowski[p_,q_]:= Sum[Subscript[g,mu,nu] Subscript[p,mu] Subscript[q,nu],{mu, 0, 3},{nu,0,3}];
+dotMinkowski[p_, q_] := Sum[Subscript[g, mu, nu] Subscript[p, mu] Subscript[q, nu], {mu, 0, 3}, {nu,0,3}];
 
 
 jAngular::usage=
-	"Spin-1 angular momentum operators";
+"  jAngular[i], i = 1, 2, 3
+Spin-1 angular momentum operators";
 jAngular[1] =  {{0,0,0},{0,0,-I},{0,I,0}};
 jAngular[2] =  {{0,0,I},{0,0,0},{-I,0,0}};
 jAngular[3] = {{0,-I,0},{I,0,0},{0,0,0}};
+jAngularVector::usage=
+	"   jAngularVector[[i]]";
+jAngularVector:= Table[jAngular[k],{k,3}];
 
 
-varepsilon[theta_, phi_,helicity_]:=
-		Switch[helicity,
-		+1,
-			Return[-1/Sqrt[2] {Cos[theta/2]^2 - Exp[2 I phi] Sin[theta/2]^2,
-			                  +I(Cos[theta/2]^2 + Exp[2 I phi] Sin[theta/2]^2),
-						      - Exp[I phi] Sin[theta]}
-			],
-		-1,
-			Return[+1/Sqrt[2] {Cos[theta/2]^2 - Exp[-2 I phi] Sin[theta/2]^2,
-			                  -I(Cos[theta/2]^2 + Exp[-2 I phi] Sin[theta/2]^2),
-						      - Exp[-I phi] Sin[theta]}
-			],
+cepsilonFirst::usage=
+"\t cepsilonFirst[helicity] -- Returns the polarization 3-vector of particle with spin 1 and helictity _helicity_ \
+along positive z-axis direction.
+\t cepsilonFirst[theta, phi, helicity] -- Returns the polarization 3-vector of particle with spin 1 and helictity _helicity_ \
+along positive (theta, phi) axis direction.";
+
+cepsilonFirst[t_]:=
+	Switch[t,
 		0,
-			Return[{Sin[theta]Cos[phi], Sin[theta]Sin[phi], Cos[theta]}],
-		_,
-			Message[varepsilon::invhel, helicity]
-		];
-varepsilon::invhel="Helicity `1` is not equal to +1, 0, -1";
+			{0, 0, 1},
+		+1,
+			1/Sqrt[2] * (jAngular[1] + I jAngular[2]) . cepsilonFirst[0],
+		-1,
+			1/Sqrt[2] * (jAngular[1] - I jAngular[2]) . cepsilonFirst[0]];
 
-
-Superscript[epsilonSecond[mass_, s_, theta_, phi_, helicity_],mu_] :=
-	If[helicity != 0,
-		If[mu == 0,
-			Return[0],
-			Return[varepsilon[theta, phi,-helicity][[mu]]]
-		],
-		Block[
-			{
-			 p0 = (s + mass^2)/(2Sqrt[s]),
-			 pAbs = (s - mass^2)/(2Sqrt[s])
-			},
-			 If[mu == 0,
-				Return[pAbs/mass],
-				Return[p0 varepsilon[theta, phi, 0][[mu]]/mass]
-			 ]
-		]
+cepsilonFirst[theta_, phi_, t_]:=
+	Return[MatrixExp[-I phi jAngular[3]]
+		 . MatrixExp[-I theta jAngular[2]]
+		 . MatrixExp[+I phi jAngular[3]] . cepsilonFirst[t]
+	//.{Cos[phi] -> TrigToExp[Cos[phi]], Sin[phi] -> TrigToExp[Sin[phi]]}
+	//Simplify 
 	];
 
 
-Superscript[raritaOut[mDelta_, s_, theta_, phi_, helicity_],mu_]:=
-	Switch[helicity,
+cepsilonSecond::usage=
+"\t cepsilonSecond[helicity] -- Returns the polarization 3-vector of particle with spin 1 and helictity _helicity_ \
+along negative z-axis direction.
+\t cepsilonSecond[theta, phi, helicity] -- Returns the polarization 3-vector of particle with spin 1 and helictity _helicity_ \
+along negative (theta, phi) axis direction.";
+
+cepsilonSecond[t_]:=
+	Return[MatrixExp[-I Pi jAngular[2]]
+		 . cepsilonFirst[t]
+	//Simplify
+	];
+cepsilonSecond[theta_, phi_, t_]:=
+	Return[MatrixExp[-I phi jAngular[3]]
+		 . MatrixExp[-I theta jAngular[2]]
+		 . MatrixExp[+I phi jAngular[3]] . cepsilonSecond[t]
+	//.{Cos[phi] -> TrigToExp[Cos[phi]], Sin[phi] -> TrigToExp[Sin[phi]]}
+	//Simplify 
+	];
+
+
+epsilonFirst::usage=
+"\t Subscript[epsilonFirst[mass, s, theta, phi, helicity], mu]";
+
+Subscript[epsilonFirst[m_, s_, theta_, phi_, t_], mu_]:=
+	If[t != 0,
+		If[mu == 0,
+			Return[0],
+			Return[cepsilonFirst[theta, phi, t][[mu]]]
+		],
+		Block[
+			{
+			 p0 = (s + m^2)/(2Sqrt[s]),
+			 pAbs = (s - m^2)/(2Sqrt[s])
+			},
+			 If[mu == 0,
+				Return[pAbs/m],
+				Return[p0/m cepsilonFirst[theta, phi, 0][[mu]]]
+			 ]
+		]
+	];
+			
+
+
+epsilonSecond::usage=
+"\t Subscript[epsilonSecond[mass, s, theta, phi, helicity],mu]";
+Subscript[epsilonSecond[m_, s_, theta_, phi_, t_], mu_]:=
+	If[t != 0,
+		If[mu == 0,
+			Return[0],
+			Return[cepsilonSecond[theta, phi, t][[mu]]]
+		],
+		Block[
+			{
+			 p0 = (s + m^2)/(2Sqrt[s]),
+			 pAbs = (s - m^2)/(2Sqrt[s])
+			},
+			 If[mu == 0,
+				Return[pAbs/m],
+				Return[p0/m cepsilonSecond[theta, phi, 0][[mu]]]
+			 ]
+		]
+	];
+		
+
+
+conj[x_]:= Refine[Conjugate[x]];
+
+
+epsilonSecondConjugate::usage=
+"\t Subscript[epsilonSecondConjugate[mass, s, theta, phi, helicity],mu]";
+Subscript[epsilonSecondConjugate[m_, s_, theta_, phi_, t_], mu_]:=
+	conj[Subscript[epsilonSecond[m, s, theta, phi, t], mu]];
+
+
+raritaOut::usage=
+	"  Subscript[raritaOut[mDelta, s, theta, phi, helicity],mu]";
+
+Subscript[raritaOut[md_, s_, theta_, phi_, t_], mu_]:=
+	Return[(-1)^(3/2 - t) *
+	 Switch[t,
 	+3/2,
-		Superscript[epsilonSecond[mDelta,s, theta, phi, +1],mu]*nucleonOut[mDelta, s, theta, phi, +1/2],
+		(Subscript[epsilonSecond[md, s, theta, phi, +1],mu]
+			*(-1)^(1/2 - 1/2) nucleonOut[md, s, theta, phi, +1/2]),
 	+1/2,
-		Sqrt[2/3] Superscript[epsilonSecond[mDelta,s, theta, phi, 0],mu]*nucleonOut[mDelta, s, theta, phi, 1/2]
-	   +Sqrt[1/3] Superscript[epsilonSecond[mDelta,s, theta, phi, +1],mu]*nucleonOut[mDelta, s, theta, phi, -1/2],	
+		(Sqrt[2/3] Subscript[epsilonSecond[md, s, theta, phi, 0],mu]
+			*(-1)^(1/2 - 1/2) nucleonOut[md, s, theta, phi, 1/2]
+	   + Sqrt[1/3] Subscript[epsilonSecond[md,s, theta, phi, +1],mu]
+			*(-1)^(1/2 + 1/2) nucleonOut[md, s, theta, phi, -1/2]),	
 	-1/2,
-		Sqrt[2/3] Superscript[epsilonSecond[mDelta,s, theta, phi, 0],mu]*nucleonOut[mDelta, s, theta, phi, -1/2]
-	   +Sqrt[1/3] Superscript[epsilonSecond[mDelta,s, theta, phi, -1],mu]*nucleonOut[mDelta, s, theta, phi, +1/2],
+		(Sqrt[2/3] Subscript[epsilonSecond[md,s, theta, phi, 0],mu]
+			*(-1)^(1/2 + 1/2) nucleonOut[md, s, theta, phi, -1/2]
+	   + Sqrt[1/3] Subscript[epsilonSecond[md, s, theta, phi, -1],mu]
+			*(-1)^(1/2 - 1/2) nucleonOut[md, s, theta, phi, +1/2]),
 	-3/2,
-		Superscript[epsilonSecond[mDelta,s, theta, phi, -1],mu]*nucleonOut[mDelta, s, theta, phi, -1/2],
-	_,
-		Message[raritaTwo::invhel, helicity]
-	]
-raritaOut::invhel="Helicity `1` is not equal to +1, 0, -1";
-
-Superscript[raritaIn[mDelta_, s_, theta_, phi_, helicity_],mu_]:=
-	Superscript[raritaOut[mDelta, s, theta, phi, helicity],mu];
+		(Subscript[epsilonSecond[md, s, theta, phi, -1],mu]
+			* (-1)^(1/2 + 1/2) nucleonOut[md, s, theta, phi, -1/2])
+		]
+	//Simplify
+	];
 
 
-(*Subscript[tDelta[s, theta_, phi_], hOut_, tau_, hIn_, lambdaIn_]:=
-*)
+(*raritaIn::usage=
+	"  Subscript[raritaIn[mDelta, s, theta, phi, helicity],mu]";
+Subscript[raritaIn[mDelta_, s_, theta_, phi_, helicity_],mu_]:=
+	Subscript[raritaOut[mDelta, s, theta, phi, helicity],mu];*)
+
+
+raritaOutConjugate::usage=
+	"  Subscript[raritaOutConjugate[mDelta, s, theta, phi, helicity],mu]";
+
+Subscript[raritaOutConjugate[md_, s_, theta_, phi_, t_], mu_]:=
+	Return[(-1)^(3/2 - t) *
+	 Switch[t,
+	+3/2,
+		(Subscript[epsilonSecondConjugate[md, s, theta, phi, +1],mu]
+			*(-1)^(1/2 - 1/2) conj[nucleonOut[md, s, theta, phi, +1/2]]),
+	+1/2,
+		(Sqrt[2/3] Subscript[epsilonSecondConjugate[md, s, theta, phi, 0],mu]
+			*(-1)^(1/2 - 1/2) conj[nucleonOut[md, s, theta, phi, 1/2]]
+	   + Sqrt[1/3] Subscript[epsilonSecondConjugate[md,s, theta, phi, +1],mu]
+			*(-1)^(1/2 + 1/2) conj[nucleonOut[md, s, theta, phi, -1/2]]),	
+	-1/2,
+		(Sqrt[2/3] Subscript[epsilonSecondConjugate[md,s, theta, phi, 0],mu]
+			*(-1)^(1/2 + 1/2) conj[nucleonOut[md, s, theta, phi, -1/2]]
+	   + Sqrt[1/3] Subscript[epsilonSecondConjugate[md, s, theta, phi, -1],mu]
+			*(-1)^(1/2 - 1/2) conj[nucleonOut[md, s, theta, phi, +1/2]]),
+	-3/2,
+		(Subscript[epsilonSecondConjugate[md, s, theta, phi, -1],mu]
+			* (-1)^(1/2 + 1/2) conj[nucleonOut[md, s, theta, phi, -1/2]])
+		]
+	//Simplify
+	];
+
+
+leptonCurrent::usage=
+	"  Subscript[leptonCurrent[mOut_, mIn_, s_, theta_, phi_, h_], mu_]";
+Subscript[leptonCurrent[mOut_, mIn_, s_, theta_, phi_, h_], mu_]:=
+	Simplify[(Conjugate[leptonOut[mOut, s, theta, phi, h]].Subscript[gamma, 0].Subscript[gamma, mu].leptonIn[mIn,s,h])];
